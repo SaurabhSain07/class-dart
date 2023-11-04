0b63d77b741c46da2b361b35f9aae02c046dfa25
Saurabh Sain <visual.sain100486@gmail.com>
2023-11-04 17:11:34 +0530

    update

---
+++ class1.dart
@@ -0,0 +1,11 @@
+void main() {
+  print("It is Program for Even Number");
+  print("---------");
+
+  int number = 22;
+  if (number % 2 == 0) {
+    print("It is Even Number");
+  } else {
+    print("It is Odd Number");
+  }
+}
---
+++ class2.dart
@@ -0,0 +1,11 @@
+void main() {
+  print("It's Program for Leep Year");
+  print("-------------");
+
+  int number = 2019;
+  if (number % 4 == 0) {
+    print("It Year Is Leep Year");
+  } else {
+    print("It Year is not a leep Year");
+  }
+}
